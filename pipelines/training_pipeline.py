485cf5eebf1702719f98b1f53a6ce57bd2aa4056
Nithira Jayarathne <hacnjayarathne@gmail.com>
2025-09-05 11:02:32 +0530

    Added model training

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -2,13 +2,13 @@ import os
 import sys
 import logging
 import pandas as pd
-import pickle
+import joblib
 from typing import Dict, Any, Tuple, Optional
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 
 from model_building import ModelFactory
 from model_training import ModelTrainer
-from model_evaluation import ModelEvaluator
+# from model_evaluation import ModelEvaluator
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
 from config import get_model_config
 logging.basicConfig(level=logging.INFO, format=
